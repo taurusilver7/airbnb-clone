d6f851a6091b37a2841ff01a884d91dde03e5cce
taurusilver7 <prasad20697@gmail.com>
2021-02-10 23:02:03 +0530

    Footer component added

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./Footer.css"
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            <p>© 2021 Airbnb Clone, No rights reserved. This is a demo.<br /> ~Privacy ~Terms ~Site ~Map</p>
+        </div>
+    )
+}
+
+export default Footer;
