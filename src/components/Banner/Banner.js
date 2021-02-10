5990e925c8eec138c8834a8c0bb1d4093ee1c437
taurusilver7 <prasad20697@gmail.com>
2021-02-10 23:01:01 +0530

    banner compnent added

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import "./Banner.css";
+
+import Search from "../Search/Search";
+
+import { Button } from "@material-ui/core";
+import { useHistory } from "react-router-dom";
+
+const Banner = () => {
+  const [showSearch, setShowSearch] = useState(false);
+  const history = useHistory();
+
+  return (
+    <div className="banner">
+      <div className="banner__search">
+        {showSearch && <Search />}
+        <Button
+          onClick={() => setShowSearch(!showSearch)}
+          className="banner__searchBtn"
+          variant="outlined"
+        >
+          {showSearch ? "Hide" : "Search Dates"}
+        </Button>
+      </div>
+      <div className="banner__info">
+        <h1>Get out & stretch your imagination.</h1>
+        <h5>
+          Plan a different kind of getaway to uncover the hidden gems near you.
+        </h5>
+        <Button onClick={() => history.push("/search")} variant="outlined">
+          Explore Nearby
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
